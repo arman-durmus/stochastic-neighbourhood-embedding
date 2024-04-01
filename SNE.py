dbcd8a6019bb9ea09f1f064be76767e29d7cc353
armad00 <armad00@mi.fu-berlin.de>
2024-04-01 02:00:11 +0200

    add: symmetrical similarity matrix, t-distributed gradient implementation for tSNE

--- SNE.py
+++ SNE.py
@@ -17,9 +17,31 @@ def calculate_similarity(data, index, std):
             similarity[j] = new_sim
     return similarity
 
+def calculate_similarity_y(data):
+    similarity = np.zeros((len(data), len(data)))
+    sim_sum = sum(
+                    np.exp(-(np.linalg.norm(data[l] - data[k]) ** 2))
+                    for k in range(len(data))
+                    for l in range(len(data))
+                    if k != l
+                )
+    for i in range(len(data)):
+        for j in range(len(data)):
+            if i == j:
+                # We only care about pairwise similarities between different points
+                similarity[i][j] = 0
+            else:
+                new_sim = np.exp(-(np.linalg.norm(data[i] - data[j]) ** 2)) / sim_sum
+                similarity[i][j] = new_sim
+    return similarity
+
 def get_gradient(sim_X, sim_Y, Y, i):
     n = len(Y)
-    return 2*sum((sim_X[i][j] + sim_X[j][i] - sim_Y[i][j] - sim_Y[j][i])*(Y[i]-Y[j]) for j in range(n))
+    return 2*sum((sim_X[i][j] + sim_X[j][i] - sim_Y[i][j] - sim_Y[j][i])*(Y[i] - Y[j]) for j in range(n))
+
+def get_gradient_t_distributed(sim_X, sim_Y, Y, i):
+    n = len(Y)
+    return 4*sum((sim_X[i][j] - sim_Y[i][j]) * (Y[i] - Y[j]) / (1 + np.linalg.norm(Y[i] - Y[j]) ** 2) for j in range(n))
 
 def kullback_leibler(sim_x, sim_y):
     assert len(sim_x) == len(sim_y)
@@ -31,10 +53,20 @@ def kullback_leibler(sim_x, sim_y):
     
     return kl_divergence
 
+def kullback_leibler_2d(sim_x, sim_y):
+    assert sim_x.shape == sim_y.shape
+    
+    kl_divergence = 0
+    for i in range(len(sim_x)):
+        for j in range(len(sim_x[0])):
+            if sim_x[i][j] != 0 and sim_y[i][j] != 0:
+                kl_divergence += sim_x[i][j] * np.log(sim_x[i][j] / sim_y[i][j])
+    
+    return kl_divergence
+
 def SNE(data, y_dim, num_iters, learning_rate):
     n = len(data)
     similarity_X = []
-    std = np.zeros(n)
     for i in range(n):
         similarity_X.append(calculate_similarity(data, i, 10))
     similarity_X = np.array(similarity_X)
@@ -42,7 +74,6 @@ def SNE(data, y_dim, num_iters, learning_rate):
     Y = np.random.normal(loc=0, scale=0.0001, size=(n, y_dim))
     for iteration in range(num_iters):
         print("iteration ", iteration)
-        # similarity_Y = np.array([calculate_similarity(Y, i, 1/np.sqrt(2)) for i in range(n)])
         similarity_Y = []
         for i in range(n):
             similarity_Y.append(calculate_similarity(Y, i, 1/np.sqrt(2)))
@@ -59,36 +90,28 @@ def SNE(data, y_dim, num_iters, learning_rate):
 def tSNE(data, y_dim, num_iters, learning_rate):
     n = len(data)
     similarity_X = []
-    std = np.zeros(n)
     for i in range(n):
         similarity_X.append(calculate_similarity(data, i, 10))
     similarity_X = np.array(similarity_X)
     similarity_X = np.add(similarity_X, np.transpose(similarity_X)) / (2 * n)
-    print(sum(sum(similarity_X)))
     Y = np.random.normal(loc=0, scale=0.0001, size=(n, y_dim))
     for iteration in range(num_iters):
-        # change this (y imilarities calculated differently)
         print("iteration ", iteration)
-        # similarity_Y = np.array([calculate_similarity(Y, i, 1/np.sqrt(2)) for i in range(n)])
-        similarity_Y = []
-        for i in range(n):
-            similarity_Y.append(calculate_similarity(Y, i, 1/np.sqrt(2)))
-        similarity_Y = np.array(similarity_Y)
-        print(similarity_Y)
+        similarity_Y = calculate_similarity_y(Y)
+        print('sum of Y similiarities:', np.sum(similarity_Y))
         
-        #print(kullback_leibler(similarity_X, similarity_Y))
         gradient = []
         for i in range(n):
-            gradient.append(get_gradient(similarity_X, similarity_Y, Y, i))
+            gradient.append(get_gradient_t_distributed(similarity_X, similarity_Y, Y, i))
         gradient = np.array(gradient)
-        print(sum(kullback_leibler(similarity_X[i], similarity_Y[i]) for i in range(n)))
+        print('Kullback-Leibler Divergence:', kullback_leibler_2d(similarity_X, similarity_Y))
         Y = Y-learning_rate*gradient
     return Y
 
 if __name__ == "__main__":
     noisy_circles_df, noisy_moons_df, blobs_df, aniso, varied_df = generate_datasets(50)
     blobs = np.array(blobs_df[["a", "b"]])
-    Y = SNE(blobs, y_dim=1, num_iters=100, learning_rate=0.5)
+    Y = tSNE(blobs, y_dim=1, num_iters=500, learning_rate=0.5)
     plt.figure()
     ax = plt.subplot()
     plt.scatter(Y.T[0], np.zeros(50), c=blobs_df.target)
